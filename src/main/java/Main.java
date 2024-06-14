1e46ef1b0adc801c04ac05e0e13f45a3f24f4931
taisiyavorontsova <taistvardovski@mail.ru>
2024-06-15 00:52:03 +0400

    Проверочная работа 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,51 @@
-public class Main {
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Car> cars = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            while (true) {
+                System.out.println("Введите марку машины");
+                String model = scan.next();
+                System.out.println("Введите скорость машины");
+                Integer speed = scan.nextInt();
+                if (speed >= 240 || speed <= 0) {
+                    System.out.println("Неверное введение скорости. Пожалуйста, введите заново.");
+                } else {
+                    cars.add(new Car(model, speed));
+                    break;
+                }
+            }
+        }
+        Speed speed = new Speed();
+        speed.speedCalculation(cars);
+    }
+}
+
+class Car {
+    String model;
+    int speed;
+
+    public Car(String model, int speed) {
+        this.model = model;
+        this.speed = speed;
+    }
+}
+
+class Speed {
+    public void speedCalculation(ArrayList<Car> cars){
+        if((cars.get(0).speed * 24) > (cars.get(1).speed * 24) && (cars.get(0).speed * 24) >(cars.get(2).speed * 24)) {
+            System.out.println("Самая быстрая машина:" + cars.get(0).model);
+        }
+        else if((cars.get(1).speed * 24) > (cars.get(0).speed * 24) && (cars.get(1).speed * 24) >(cars.get(2).speed * 24)) {
+            System.out.println("Самая быстрая машина:" + cars.get(1).model);
+        }
+        else if((cars.get(2).speed * 24) > (cars.get(0).speed * 24) && (cars.get(2).speed * 24) >(cars.get(1).speed * 24)) {
+            System.out.println("Самая быстрая машина:" + cars.get(2).model);
+        }
     }
 }
